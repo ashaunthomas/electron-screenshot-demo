09342e3236b24964611df7606a8198237ee1b52f
Ashaun Thomas <ashaunthomas@siu.edu>
2017-09-29 15:59:52 -0500

    scaffolding

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 const electron = require('electron');
+
+const { app } = electron;
+
+let mainWindow;
+
+app.on('ready', _ => {
+    mainWindow = new BrowserWindow({
+        width:0,
+        height:0,
+        resizable:false,
+        frame:false
+    })
+
+    mainWindow.loadURL(`file://${__dirname}/capture.html`);
+    mainWindow.on('close', _ => {
+        mainWindow = null;
+    })
+})
